7f23b80ba549d8932e1a0912037964341c248831
bgrana <bgrana@conwet.com>
2014-09-11 11:09:45 +0200

    Add placeholder to show when no video has been received

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 window.onload = function() {
     "use strict";
 
-    var changeURL = function changeURL(url) {
-        var videoOutput = document.getElementById("videoOutput");
+    function recv_video(url) {
+    	var body = document.getElementByTagName('body');
+    	while (body.firstChild) {
+    		body.firstChild.remove();
+    	}
+    	var videoOutput = document.createElement('video');
+    	videoOutput.id = 'videoOutput';
+    	videoOutput.className = 'video';
         videoOutput.src = url;
+    }
 
-    };
-    MashupPlatform.wiring.registerCallback('url', changeURL);
+    MashupPlatform.wiring.registerCallback('url', recv_video);
 
 };
