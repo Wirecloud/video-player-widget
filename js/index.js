46a693eb28ca1bc6cab99756b784fb0ea63043b0
bgrana <bgrana@conwet.com>
2014-09-12 10:28:34 +0200

    Fixed bug at receiving a wiring event

--- js/index.js
+++ js/index.js
@@ -2,14 +2,18 @@ window.onload = function() {
     "use strict";
 
     function recv_video(url) {
-    	var body = document.getElementByTagName('body');
-    	while (body.firstChild) {
-    		body.firstChild.remove();
+    	var container = document.getElementById('container');
+    	while (container.hasChildNodes()) {
+    		container.firstChild.remove();
     	}
     	var videoOutput = document.createElement('video');
     	videoOutput.id = 'videoOutput';
-    	videoOutput.className = 'video';
         videoOutput.src = url;
+    	videoOutput.className = 'video';
+        videoOutput.autoplay = true;
+        videoOutput.controls = false;
+        document.getElementsByTagName('body').className = '';
+        container.appendChild(videoOutput);
     }
 
     MashupPlatform.wiring.registerCallback('url', recv_video);
