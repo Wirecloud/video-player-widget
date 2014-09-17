534156436735ea84a8ff191ee8dbd7c9cb7f4706
bgrana <bgrana@conwet.com>
2014-09-17 13:52:12 +0200

    Implement terminate_stream event

--- js/index.js
+++ js/index.js
@@ -1,21 +1,60 @@
 window.onload = function() {
     "use strict";
 
-    function recv_video(url) {
+    function recv_video(data) {
+        
+        data = data.split(' ');
+        var username = data[0];
+        var url = data[1];
+
     	var container = document.getElementById('container');
-    	while (container.hasChildNodes()) {
+    	var videoOutput = create_video(url);
+        //var nav_bar = document.createElement('nav');
+
+        while (container.hasChildNodes()) {
     		container.firstChild.remove();
     	}
-    	var videoOutput = document.createElement('video');
-    	videoOutput.id = 'videoOutput';
-        videoOutput.src = url;
-    	videoOutput.className = 'video';
-        videoOutput.autoplay = true;
-        videoOutput.controls = false;
-        document.getElementsByTagName('body')[0].className = '';
+
+        //nav_bar.id = 'top-bar';
+        //nav_bar.className = 'top-bar';
+
+        
         container.appendChild(videoOutput);
+        //container.appendChild(nav_bar);
+    }
+
+    function create_video (url) {
+        
+        var videoOutput = document.createElement('video');
+
+        videoOutput.id = 'videoOutput';
+        videoOutput.src = url;
+        videoOutput.className = 'video';
+        videoOutput.autoplay = true;
+        videoOutput.controls = true;
+        document.getElementsByTagName('body')[0].className = '';
+
+        return videoOutput;
+    }
+
+    function terminate_stream () {
+        
+        var container = document.getElementById('container');
+        var placeholder = document.createElement('span');
+        
+        placeholder.className = 'placeholder';
+        placeholder.textContent = 'No video received yet!';
+        document.getElementsByTagName('body')[0].className = 'static';
+
+        while (container.hasChildNodes()) {
+            container.firstChild.remove();
+        }
+
+        container.appendChild(placeholder);
+
     }
 
     MashupPlatform.wiring.registerCallback('url', recv_video);
+    MashupPlatform.wiring.registerCallback('terminate_stream', terminate_stream)
 
 };
