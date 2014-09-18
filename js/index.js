778c43435004e30831ae333c0cd9b1f20d3a1e12
bgrana <bgrana@conwet.com>
2014-09-18 12:23:07 +0200

    Refactor and fix bugs

--- js/index.js
+++ js/index.js
@@ -1,29 +1,44 @@
-window.onload = function() {
-    "use strict";
+var VideoPlayer = function () {
 
-    function recv_video(data) {
+    this.username = '';
+    this.url = '';
+
+    MashupPlatform.wiring.registerCallback('url', this.recv_video.bind(this));
+    MashupPlatform.wiring.registerCallback('terminate_stream', this.terminate_stream.bind(this));
+};
+
+VideoPlayer.prototype = {
+
+    constructor: VideoPlayer,
+
+
+    recv_video: function (data) {
         
         data = data.split(' ');
-        var username = data[0];
-        var url = data[1];
+        this.username = data[0];
+        this.url = data[1];
 
-    	var container = document.getElementById('container');
-    	var videoOutput = create_video(url);
-        //var nav_bar = document.createElement('nav');
+        var container = document.getElementById('container');
+        var videoOutput = this.create_video(this.url);
 
-        while (container.hasChildNodes()) {
-    		container.firstChild.remove();
-    	}
-
-        //nav_bar.id = 'top-bar';
-        //nav_bar.className = 'top-bar';
-
+        this.remove_child_nodes(container);
         
         container.appendChild(videoOutput);
-        //container.appendChild(nav_bar);
-    }
+    },
 
-    function create_video (url) {
+
+    create_navbar: function () { //TODO
+        
+        var nav_bar = document.createElement('nav');
+
+        nav_bar.id = 'top-bar';
+        nav_bar.className = 'top-bar';
+
+        container.appendChild(nav_bar);
+    },
+
+
+    create_video: function (url) {
         
         var videoOutput = document.createElement('video');
 
@@ -35,26 +50,36 @@ window.onload = function() {
         document.getElementsByTagName('body')[0].className = '';
 
         return videoOutput;
-    }
+    },
 
-    function terminate_stream () {
+
+    terminate_stream: function () {
         
-        var container = document.getElementById('container');
+        this.username = '';
+        this.url = '';
+        var container = document.getElementById('container'); 
+        var placeholder = this.create_placeholder();
+
+        this.remove_child_nodes(container);
+
+        container.appendChild(placeholder);
+    },
+
+    create_placeholder: function () {
+
         var placeholder = document.createElement('span');
-        
+
         placeholder.className = 'placeholder';
         placeholder.textContent = 'No video received yet!';
         document.getElementsByTagName('body')[0].className = 'static';
 
+        return placeholder;
+    },
+
+    remove_child_nodes: function (container) {
+        
         while (container.hasChildNodes()) {
             container.firstChild.remove();
         }
-
-        container.appendChild(placeholder);
-
     }
-
-    MashupPlatform.wiring.registerCallback('url', recv_video);
-    MashupPlatform.wiring.registerCallback('terminate_stream', terminate_stream)
-
 };
