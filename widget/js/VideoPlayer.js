4aef84db0a3b3b39017b5c122a9e07e8f43666e4
bgrana <bgrana@conwet.com>
2015-04-09 16:53:33 +0200

    Small code refactor

--- widget/js/VideoPlayer.js
+++ widget/js/VideoPlayer.js
@@ -1,69 +1,82 @@
-var VideoPlayer = function () {
+var VideoPlayer = (function () {
+    "use strict";
 
-    this.url = '';
+    /*** CONSTRUCTOR ***/
+    var VideoPlayer = function () {
 
-    MashupPlatform.wiring.registerCallback('url', this.recv_video.bind(this));
-};
-
-VideoPlayer.prototype = {
-
-    constructor: VideoPlayer,
-
-
-    recv_video: function (url) {
-        
-        this.url = url;
-
-        var container = document.getElementById('container');
-        var videoOutput = this.create_video(this.url);
-
-        this.remove_child_nodes(container);
-        
-        container.appendChild(videoOutput);
-    },
-
-
-    create_video: function (url) {
-        
-        var videoOutput = document.createElement('video');
-
-        videoOutput.id = 'videoOutput';
-        videoOutput.src = url;
-        videoOutput.className = 'video';
-        videoOutput.autoplay = true;
-        videoOutput.controls = true;
-        document.body.className = '';
-
-        return videoOutput;
-    },
-
-
-    terminate_stream: function () {
-        
         this.url = '';
-        var container = document.getElementById('container'); 
-        var placeholder = this.create_placeholder();
 
-        this.remove_child_nodes(container);
+        MashupPlatform.wiring.registerCallback('url', this.recv_video.bind(this));
+    };
 
-        container.appendChild(placeholder);
-    },
 
-    create_placeholder: function () {
+    /*** PUBLIC ***/
+    VideoPlayer.prototype = {
 
-        var placeholder = document.createElement('span');
+        constructor: VideoPlayer,
 
-        placeholder.className = 'placeholder';
-        placeholder.textContent = 'No video received yet!';
-        document.body.className = 'static';
 
-        return placeholder;
-    },
+        recv_video: function (url) {
+            
+            this.url = url;
 
-    remove_child_nodes: function (container) {
-        
+            var container = document.getElementById('container');
+            var videoOutput = this.create_video(this.url);
+
+            remove_child_nodes(container);
+            
+            container.appendChild(videoOutput);
+        },
+
+
+        create_video: function (url) {
+            
+            var videoOutput = document.createElement('video');
+
+            videoOutput.id = 'videoOutput';
+            videoOutput.src = url;
+            videoOutput.className = 'video';
+            videoOutput.autoplay = true;
+            videoOutput.controls = true;
+            document.body.className = '';
+
+            return videoOutput;
+        },
+
+
+        terminate_stream: function () {
+            
+            this.url = '';
+            var container = document.getElementById('container'); 
+            var placeholder = this.create_placeholder();
+
+            remove_child_nodes(container);
+
+            container.appendChild(placeholder);
+        },
+
+        create_placeholder: function () {
+
+            var placeholder = document.createElement('span');
+
+            placeholder.className = 'placeholder';
+            placeholder.textContent = 'No video received yet!';
+            document.body.className = 'static';
+
+            return placeholder;
+        }
+    };
+
+
+    /*** PRIVATE ***/
+    function remove_child_nodes (container) {
+            
         while (container.hasChildNodes()) {
             container.firstChild.remove();
         }
     }
-};
+
+
+    return VideoPlayer;
+
+})();
