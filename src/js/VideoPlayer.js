92799fe31af00cda8924dce9e6bcfe5d4644fff3
Alejandro Rodriguez <alex.rodriguez@conwet.com>
2016-12-19 13:06:40 +0100

    Update repo structure

--- src/js/VideoPlayer.js
+++ src/js/VideoPlayer.js
@@ -1,34 +1,34 @@
-var VideoPlayer = (function () {
+(function () {
     "use strict";
 
-    /*** CONSTRUCTOR ***/
+    /** * CONSTRUCTOR ***/
     var VideoPlayer = function () {
         MashupPlatform.wiring.registerCallback('url', this.recv_video.bind(this));
         this.recv_video(MashupPlatform.prefs.get('initial-url'));
     };
 
 
-    /*** PUBLIC ***/
+    /** * PUBLIC ***/
     VideoPlayer.prototype = {
 
         constructor: VideoPlayer,
 
 
         recv_video: function (url) {
-            
+
             this.url = url;
 
             var container = document.getElementById('container');
             var videoOutput = this.create_video(this.url);
 
             remove_child_nodes(container);
-            
+
             container.appendChild(videoOutput);
         },
 
 
         create_video: function (url) {
-            
+
             var videoOutput = document.createElement('video');
 
             videoOutput.id = 'videoOutput';
@@ -43,9 +43,9 @@ var VideoPlayer = (function () {
 
 
         terminate_stream: function () {
-            
+
             this.url = '';
-            var container = document.getElementById('container'); 
+            var container = document.getElementById('container');
             var placeholder = this.create_placeholder();
 
             remove_child_nodes(container);
@@ -66,9 +66,9 @@ var VideoPlayer = (function () {
     };
 
 
-    /*** PRIVATE ***/
-    function remove_child_nodes (container) {
-            
+    /** * PRIVATE ***/
+    var remove_child_nodes = function remove_child_nodes(container) {
+
         while (container.hasChildNodes()) {
             container.firstChild.remove();
         }
